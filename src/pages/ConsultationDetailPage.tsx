fa8903a585de548c551cfe1bb34c034557a036fe
Julie Groth <groth.julie@gmail.com>
2021-03-05 21:22:14 -0800

    add routes and main pages

---
+++ src/pages/ConsultationDetailPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConsultationDetailPage: React.FC<{}> = ({}) => {
+  return <h1>Detail Page</h1>;
+};
+
+export default ConsultationDetailPage;
---
+++ src/pages/ConsultationListPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConsultationListPage: React.FC<{}> = ({}) => {
+  return <h1>List Page</h1>;
+};
+
+export default ConsultationListPage;
